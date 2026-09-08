c6c1f4305b0cefc0d12424e25968eb99e51d9dd3
Carlos Chau <carlos.chau@aacat.com>
2026-09-08 17:40:05 +0800

    update style

--- src/App.tsx
+++ src/App.tsx
@@ -18,15 +18,15 @@ function App() {
     }
 
     const expectedMove = stockPrice * iv *0.01 * Math.sqrt(dte / 365)
-    setExpectedMove(expectedMove)
-    setMoveUpPrice(stockPrice + expectedMove)
-    setMoveDownPrice(stockPrice - expectedMove)
+    setExpectedMove(+expectedMove.toFixed(2))
+    setMoveUpPrice(+(stockPrice + expectedMove).toFixed(2))
+    setMoveDownPrice(+(stockPrice - expectedMove).toFixed(2))
   }
 
   return (
     <>
       {/* Title */}
-      <Typography variant="h4" sx={{ margin: '10px 0 50px 0'}}>Calculate Expected Price of Stock by IV</Typography>
+      <Typography variant="h4" sx={{ margin: '10px 0 30px 0'}}>Calculate Expected Price of Stock by IV</Typography>
       {/* Input form */}
       <Container>
         <Box sx={{ margin: '20px' }}>
@@ -34,13 +34,12 @@ function App() {
           <Box><TextField id="dte" label="Days to expiration" variant="standard" type="number" value={dte ?? ''} onChange={(e) => {setDte(parseInt(e.target.value) || null); calculateExpectedMove();}}/></Box>
           <Box><TextField id="implied-volatility" label="Implied Volatility" variant="standard" type="number" value={iv ?? ''} onChange={(e) => {setIv(parseFloat(e.target.value) || null); calculateExpectedMove();}}/></Box>
         </Box>
-        <Box sx={{display: 'grid', placeItems: 'center', gridAutoFlow: 'column', justifyContent: 'space-evenly', marginTop: '50px'}} >
-          <Box  >
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-evenly', padding:'20px' }} >
+          <Box sx={{ margin: '0 20px' }}>
             <Typography sx={{alignItems: 'center'}} variant="h5">Expected Move: {expectedMove}</Typography>
           </Box>
-          <Box>
+          <Box sx={{ margin: '0 20px' }}>
             <Typography variant="h5">Move Down Price: {moveDownPrice}</Typography>
-            <br/>
             <Typography variant="h5">Move Up Price: {moveUpPrice}</Typography>
           </Box>
         </Box>
